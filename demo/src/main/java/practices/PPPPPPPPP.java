0d8dd416c956fd4fc4fb16d1223d034f4570d6d4
shiva <shivaranjannagathan.n@gmail.com>
2023-08-25 11:53:29 +0530

    pattern pgm

--- demo/src/main/java/practices/PPPPPPPPP.java
+++ demo/src/main/java/practices/PPPPPPPPP.java
@@ -14,3 +14,20 @@ public class PPPPPPPPP {
 		}
 
 }
+
+
+class demo {
+	public static void main(String[] args) {
+		int n=5;
+		int i,j;
+		
+		for(i=0;i<=5;i++)
+		{
+			for(j=1;j<=4;j++)
+			{
+				System.out.println("*");
+			}
+		}
+		
+	}
+}
